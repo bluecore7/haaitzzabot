066b0690de93513081529b0bef3e00073841d2c0
bluecore <bluecorexux@gmail.com>
2026-06-29 10:25:17 +0530

    fix: Add error handling in OAuth server

--- Oauth.js
+++ Oauth.js
@@ -7,20 +7,31 @@ const port = 3000;
 
 app.get("/oauth/callback", async (req, res) => {
   const code = req.query.code;
-  const response = await axios.post(
-    "https://slack.com/api/oauth.v2.access",
-    null,
-    {
-      params: {
-        code: code,
-        client_id: process.env.SLACK_CLIENT_ID,
-        client_secret: process.env.SLACK_CLIENT_SECRET,
+  try {
+    const response = await axios.post(
+      "https://slack.com/api/oauth.v2.access",
+      null,
+      {
+        params: {
+          code: code,
+          client_id: process.env.SLACK_CLIENT_ID,
+          client_secret: process.env.SLACK_CLIENT_SECRET,
+        },
       },
-    },
-  );
-  const botToken = response.data.access_token;
-  console.log("New workspace installed! Bot Token:", botToken);
-  res.send("haaitzzabot installed! You can close this tab.");
+    );
+
+    const botToken = response.data.access_token;
+    if (botToken) {
+      console.log("New workspace installed! Bot Token:", botToken);
+
+      res.send("haaitzzabot installed! You can close this tab.");
+    } else {
+      console.log("Slack response:", JSON.stringify(response.data));
+      res.send("Token Error");
+    }
+  } catch (error) {
+    res.send("OAuth failed: something went wrong");
+  } 
 });
 
 app.listen(port, () => {
